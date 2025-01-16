ad69ef99e653c530b30b6565b5c3acd69f688295
PyaePhyoTun <pyaephyohtun201@gmail.com>
2025-01-16 13:30:00 +0630

    Update version

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, GitOpsi! We will now test it if it is acutally work"
+    return "Hello, this is working"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
