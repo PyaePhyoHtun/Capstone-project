c8a4709bc3dca87e80bc0a45865d75d60e82b30a
PyaePhyoTun <pyaephyohtun201@gmail.com>
2025-01-16 14:57:04 +0630

    Trying with github action

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "This is trying with github action"
+    return "This is trying with github action v2.0"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
