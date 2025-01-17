2ed0da7c768a31d52ca08f0f6e7e4473278d10c6
PyaePhyoTun <pyaephyohtun201@gmail.com>
2025-01-17 11:12:57 +0630

    Test with GA

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Try where is my problem"
+    return "Try without flux"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
